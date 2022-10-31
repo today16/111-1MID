7f82660340fbe47f2f66042b9aaa0416cb2973df
today16 <k101620275@yahoo.com.tw>
2022-10-31 10:09:50 +0800

    Q1OK

--- 111-1MID/Q1.aspx
+++ 111-1MID/Q1.aspx
@@ -10,7 +10,25 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <h1>線上掛號系統</h1>
+            <h2>|| 掛號資訊</h2>
+            <asp:Panel ID="pl_Info" runat="server" BorderWidth="1px" Width="500px">
+                <h3><asp:Label ID="lb_Type" runat="server" Text="初診"></asp:Label></h3>
+                <asp:RadioButton ID="rd_Id" runat="server" Text="身分證字號" Checked="True" GroupName="rb_Type"/>
+                <asp:RadioButton ID="rb_Hid" runat="server" Text="病歷號" GroupName="rb_Type" />
+                <asp:TextBox ID="tb_Account" runat="server" Height="20px" Width="400px" AutoPostBack="True" OnTextChanged="tb_Account_TextChanged"></asp:TextBox><br />
+                連絡電話：(選填)
+                <asp:TextBox ID="tb_Phone" runat="server" Height="20px" Width="300px"></asp:TextBox>
+                <asp:Button ID="btn_Submit" runat="server" Text="掛號" Visible="False" Width="80px" OnClick="btn_Submit_Click" />
+            </asp:Panel>
+            <br />
+            <asp:Panel ID="pl_Msg" runat="server" BorderWidth="1px" Visible="False" Width="500px">
+                <asp:Label ID="lb_Msg" runat="server" Text=""></asp:Label>
+                <asp:LinkButton ID="LinkButton1" runat="server"  OnClick="LinkButton1_Click"  PostBackUrl="Q1.aspx">重新填寫</asp:LinkButton>
+
+            </asp:Panel>
         </div>
     </form>
 </body>
 </html>
+
--- 111-1MID/Q1.aspx.cs
+++ 111-1MID/Q1.aspx.cs
@@ -12,5 +12,52 @@ namespace _111_1MID {
         protected void Page_Load(object sender, EventArgs e) {
 
         }
+        protected void tb_Account_TextChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < s_IdSet.Length; i++)
+            {
+                if (tb_Account.Text == s_IdSet[i] && tb_Account.Text != "")
+                {
+                    lb_Type.Text = "複診";
+                    btn_Submit.Visible = true;
+                    break;
+                }
+                else if (tb_Account.Text != s_IdSet[i] && tb_Account.Text != "")
+                {
+                    lb_Type.Text = "初診";
+                    btn_Submit.Visible = true;
+
+                }
+                else
+                {
+                    btn_Submit.Visible = false;
+                    lb_Type.Text = "初診";
+                }
+            }
+            pl_Msg.Visible = false;
+        }
+        protected void btn_Submit_Click(object sender, EventArgs e)
+        {
+            pl_Msg.Visible = true;
+            lb_Msg.Text = "";
+            if (tb_Phone.Text == "")
+            {
+                lb_Msg.Text += lb_Type.Text + "<br />" + tb_Account.Text + "先生/小姐，已完成掛號。" + "<br />" + "<br />";
+            }
+            else
+            {
+                lb_Msg.Text += lb_Type.Text + "<br />" + tb_Account.Text + "先生/小姐，已完成掛號。" + "<br />" + "連絡電話為" + tb_Phone.Text + "<br />" + "<br />";
+            }
+        }
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            lb_Type.Text = "初診";
+            rd_Id.Checked = true;
+            tb_Account.Text = "";
+            tb_Phone.Text = "";
+            btn_Submit.Visible = false;
+            pl_Msg.Visible = false;
+        }
     }
 }
